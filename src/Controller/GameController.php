53795a621080b758f5e3d6eebde574f28d8ecbbe
Rob1n59 <pruvotrobin59@gmail.com>
2025-11-25 14:27:53 +0100

    premières ajouts pour menu choix du héro

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class GameController extends AbstractController
 {
@@ -68,4 +69,79 @@ class GameController extends AbstractController
 
         return $this->redirectToRoute('combat_start');
     }
+    #[Route('/choose-hero', name: 'choose_hero')]
+public function chooseHero(): Response
+{
+    // Liste des classes disponibles
+    $classes = [
+        [
+            'id' => 1,
+            'name' => 'Guerrier',
+            'image' => '/images/classes/warrior.png',
+            'hp' => 120,
+            'attack' => 8,
+            'defense' => 12
+        ],
+        [
+            'id' => 2,
+            'name' => 'Mage',
+            'image' => '/images/classes/mage.png',
+            'hp' => 80,
+            'attack' => 15,
+            'defense' => 5
+        ],
+        [
+            'id' => 3,
+            'name' => 'Archer',
+            'image' => '/images/classes/archer.png',
+            'hp' => 100,
+            'attack' => 10,
+            'defense' => 8
+        ]
+    ];
+
+    return $this->render('game/choose_hero.html.twig', [
+        'classes' => $classes
+    ]);
+}
+    #[Route('/choose-hero/confirm', name: 'choose_hero_confirm', methods: ['POST'])]
+public function chooseHeroConfirm(
+    Request $request,
+    EntityManagerInterface $em,
+    SessionInterface $session
+): Response {
+    $classId = $request->request->get('class_id');
+    $playerName = $request->request->get('player_name');
+
+    if (!$classId || !$playerName) {
+        $this->addFlash('error', 'Veuillez choisir une classe et un nom.');
+        return $this->redirectToRoute('choose_hero');
+    }
+
+    // Données des classes (on pourrait mettre ça ailleurs)
+    $classes = [
+        1 => ['hp' => 120, 'attack' => 8, 'defense' => 12],
+        2 => ['hp' => 80, 'attack' => 15, 'defense' => 5],
+        3 => ['hp' => 100, 'attack' => 10, 'defense' => 8]
+    ];
+
+    $stats = $classes[$classId];
+
+    // Création du joueur
+    $player = new Player();
+    $player->setName($playerName);
+    $player->setHp($stats['hp']);
+    $player->setAttack($stats['attack']);
+    $player->setDefense($stats['defense']);
+    $player->setGold(0);
+    $player->setExperience(0);
+
+    $em->persist($player);
+    $em->flush();
+
+    $session->set('player_id', $player->getId());
+
+    return $this->redirectToRoute('game_explore');
+}
+
 }
